9ef89143a8598bc7da08e4ec44f4fac5e4284cce
Artem71 <fich71@mail.ru>
2019-10-17 14:07:54 +0300

    prepare

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,7 @@
 
 module.exports = {
   mode: 'universal',
-  /*
-  ** Headers of the page
-  */
+
   head: {
     title: process.env.npm_package_name || '',
     meta: [
@@ -15,40 +13,26 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  /*
-  ** Customize the progress-bar color
-  */
+
   loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
+
   css: [
     'element-ui/lib/theme-chalk/index.css'
   ],
-  /*
-  ** Plugins to load before mounting the App
-  */
+
   plugins: [
-    '@/plugins/element-ui'
+    '@/plugins/globals'
   ],
-  /*
-  ** Nuxt.js dev-modules
-  */
+
   buildModules: [
   ],
-  /*
-  ** Nuxt.js modules
-  */
+
   modules: [
   ],
-  /*
-  ** Build configuration
-  */
+
   build: {
     transpile: [/^element-ui/],
-    /*
-    ** You can extend webpack config here
-    */
+
     extend (config, ctx) {
     }
   }
