0c9a78eb3082cb37c3fd0f1cea47cd4687d33f7c
Artem71 <fich71@mail.ru>
2019-10-17 15:00:05 +0300

    main page

---
+++ components/main/Post.vue
@@ -0,0 +1,13 @@
+<template>
+  <article class="post">
+
+  </article>
+</template>
+
+<style lang="scss" scoped>
+  .post {
+    margin-bottom: 2rem;
+    height: 200px;
+    border: 1px solid black;
+  }
+</style>
