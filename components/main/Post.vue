c463d3ae56723555508947618df158dcd1668627
Artem71 <fich71@mail.ru>
2019-10-17 15:25:04 +0300

    post

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,13 +1,64 @@
 <template>
-  <article class="post">
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post"
+  >
+    <header slot="header" class="post-header">
+      <h3>Post header</h3>
 
-  </article>
+      <small>
+        <i class="el-icon-time"></i>
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
+    <div class="post-body">
+      <img
+        src="https://cdn.getyourguide.com/img/tour_img-1096032-146.jpg"
+        alt="post image"
+        class="post-img"
+      >
+    </div>
+    <el-footer class="post-footer">
+      <el-button round @click="openPost">Открыть</el-button>
+      <span>
+        <i class="el-icon-message"></i>
+        12
+      </span>
+    </el-footer>
+  </el-card>
 </template>
+
+<script>
+export default {
+  methods: {
+    openPost() {
+      console.log('open post')
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
   .post {
-    margin-bottom: 2rem;
-    height: 200px;
-    border: 1px solid black;
+    margin-bottom: 1.5rem;
+  }
+
+  .post-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .post-img {
+    width: 100%;
+    height: auto;
+  }
+
+  .post-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
   }
 </style>
