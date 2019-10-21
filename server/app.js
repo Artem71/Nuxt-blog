b1b663e14e6a4325490fd133a16fff57b8c190b9
Artem71 <fich71@mail.ru>
2019-10-21 23:40:50 +0300

    routes, contollers, models

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const authRoutes = require('./routes/auth.routes')
 const keys = require('./keys')
 const app = express()
 
@@ -8,7 +9,9 @@ mongoose.connect(keys.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(error => console.error(error))
 
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+app.use('/api/auth', authRoutes)
 
 module.exports = app
