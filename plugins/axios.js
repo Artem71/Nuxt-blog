b4908789bc5008170e0189b780bfe3f9a8b2cecd
Artem71 <fich71@mail.ru>
2019-10-24 01:41:08 +0300

    add token to request

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,14 @@
 export default function({ $axios, redirect, store }) {
+
+  $axios.interceptors.request.use(request => {
+    if (store.getters['auth/isAuth'] && !request.header.common['Authorization']) {
+      const token = store.getters['auth/token']
+      request.header.common['Authorization'] = `Bearer ${token}`
+    }
+
+    return request
+  })
+
   $axios.onError(error => {
     if (error.response) {
       if (error.response.status === 401) {
