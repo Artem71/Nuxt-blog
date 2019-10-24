5b7b4cfe77167d355d01bd37e9fcae07e2b116e5
Artem71 <fich71@mail.ru>
2019-10-24 13:22:58 +0300

    token session support

--- store/auth.js
+++ store/auth.js
@@ -1,3 +1,7 @@
+import Cookie from 'cookie'
+import Cookies from 'js-cookie'
+import jwtDecode from 'jwt-decode'
+
 export const state = () => ({
   token: null
 })
@@ -21,6 +25,20 @@ export const actions = {
       throw e
     }
   },
+  autoLogin({dispatch}) {
+    const cookieStr = process.browser
+      ? document.cookie
+      : this.app.context.req.headers.cookie
+
+    const cookies = Cookie.parse(cookieStr || '') || {}
+    const token = cookies['jwt-token']
+
+    if (isJWTValid(token)) {
+      dispatch('setToken')
+    } else {
+      dispatch('logout')
+    }
+  },
   async createUser({commit}, formData) {
     try {
       await this.$axios.$post('/api/auth/admin/create', formData)
@@ -32,14 +50,25 @@ export const actions = {
   setToken({commit}, token) {
     this.$axios.setToken(token, 'Bearer')
     commit('setToken', token)
+    Cookies.set('jwt-token', token)
   },
   logout({commit}) {
     this.$axios.setToken(false)
     commit('clearToken')
+    Cookies.remove('jwt-token')
   }
 }
 
 export const getters = {
   isAuth: state => Boolean(state.token),
   token: state => state.token
+}
+
+function isJWTValid(token) {
+  if (!token) return false
+
+  const jwtData = jwtDecode(token) || {}
+  const expires = jwtData.exp || 0
+
+  return (new Date().getTime() / 1000) < expires
 }
